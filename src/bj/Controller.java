0da416b08f95b2b256b79f58da7d1b7d1d542a82
missHS <helene.schonbeck@gmail.com>
2020-11-23 14:52:29 +0100

    Added Dealer hand

--- src/bj/Controller.java
+++ src/bj/Controller.java
@@ -5,7 +5,8 @@ import java.util.Random;
 
 public class Controller {
 
-	private ArrayList<Integer> cardHand = new ArrayList<Integer>();
+	private ArrayList<Integer> playerHand = new ArrayList<Integer>();
+	private ArrayList<Integer> dealerHand = new ArrayList<Integer>();
 
 	public int drawNewCard() {
 
@@ -22,13 +23,28 @@ public class Controller {
 
 	public void drawPlayerHand() {
 		int card = drawNewCard();
-		cardHand.add(card);
+		playerHand.add(card);
 
+	}
+	
+	public void drawDealerHand() {
+		int card = drawNewCard();
+		dealerHand.add(card);		
 	}
 
 	public String showPlayerHand() {
 		String hand = null;
-		for (int temp : cardHand) {
+		for (int temp : playerHand) {
+			hand = hand + " " + Integer.toString(temp);
+		}
+
+		hand = hand.replace("null ", "");
+		return hand;
+	}
+	
+	public String showDealerHand() {
+		String hand = null;
+		for (int temp : dealerHand) {
 			hand = hand + " " + Integer.toString(temp);
 		}
 
@@ -39,7 +55,17 @@ public class Controller {
 	public int sumOfCardsPlayerHand() {
 		int sum = 0;
 
-		for (int temp : cardHand) {
+		for (int temp : playerHand) {
+			sum = sum + temp;
+		}
+
+		return sum;
+	}
+	
+	public int sumOfCardsDealerHand() {
+		int sum = 0;
+
+		for (int temp : dealerHand) {
 			sum = sum + temp;
 		}
 
--- src/bj/Main.java
+++ src/bj/Main.java
@@ -14,19 +14,47 @@ public class Main {
 		
 		controller.drawPlayerHand();
 		controller.drawPlayerHand();
+		controller.drawDealerHand();
+		controller.drawDealerHand();
 		
-		System.out.println(controller.showPlayerHand());
+		System.out.println("This is the dealer hand = " + controller.showDealerHand());
+		System.out.println("This is your hand = " + controller.showPlayerHand());
 		
-		while(controller.sumOfCardsPlayerHand()<21)	{	
+		
+		while(controller.sumOfCardsPlayerHand()<21 && controller.sumOfCardsDealerHand()<21)	{	
 		System.out.println("Would you like a new card? Y/N");
 		String selection = input.nextLine().toUpperCase();
 		
 		if (selection.equals("Y")) {
 			controller.drawPlayerHand();
-			System.out.println(controller.showPlayerHand());			
-		}
+			controller.drawDealerHand();
+			System.out.println("This is the dealer hand = " + controller.showDealerHand());
+			System.out.println("This is your hand = " + controller.showPlayerHand());	
+					}
 		}
 		
+		if(controller.sumOfCardsPlayerHand()>21) {
+			System.out.println("you loose");
+			
+		}
+		else if(controller.sumOfCardsDealerHand()>21 && controller.sumOfCardsPlayerHand()<21)  {
+			System.out.println("you win!");
+		}
+		
+		else if(controller.sumOfCardsPlayerHand()==21) {
+			System.out.println("Black Jack");
+		}
+		
+		else if(controller.sumOfCardsPlayerHand()<controller.sumOfCardsDealerHand()) {
+			System.out.println("you loose!");
+		}
+		else if(controller.sumOfCardsDealerHand()<controller.sumOfCardsPlayerHand()) {
+			System.out.println("you win!");
+		}
+			
+		else {
+			System.out.println("Vad hände nu helene?");
+		}
 		 
 		
 		
